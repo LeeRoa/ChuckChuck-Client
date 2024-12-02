0d37f07cce77f0a7258bff785bdd8dbfde8e7b5e
김시은 <kimssi9567@gmail.com>
2024-12-02 18:17:09 +0900

    first commit

---
+++ src/test/java/com/client/chuck/ChuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.chuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
