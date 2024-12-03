a0a1d37f8b7920c7913fc5cd7d39935e7663b790
김시은 <kimssi9567@gmail.com>
2024-12-03 15:23:20 +0900

    second commit

---
+++ src/main/java/com/client/chuck/controller/mainController.java
@@ -0,0 +1,14 @@
+package com.client.chuck.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class mainController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
