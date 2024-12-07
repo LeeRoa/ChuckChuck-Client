6b77382e99838dd0fb5358367c63830d6603730e
김시은 <kimssi9567@gmail.com>
2024-12-07 17:47:29 +0900

    feat: 메인화면, 모달추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,14 +1,54 @@
 // gnb 메뉴 클릭효과
 const gnbMenus = document.querySelectorAll(".gnb-menu-btn");
 
-gnbMenus.forEach((item) => {
-    item.addEventListener("click", (e) => {
+gnbMenus.forEach((menu) => {
+    menu.addEventListener("click", (e) => {
         e.preventDefault();
-        item.classList.add("on");
-        gnbMenus.forEach((otherItem) => {
-            if (otherItem !== item) {
-                otherItem.classList.remove("on");
+        menu.classList.add("on");
+        gnbMenus.forEach((otherMenu) => {
+            if (otherMenu !== menu) {
+                otherMenu.classList.remove("on");
             }
+        })
+    })
+})
+
+// 헤더 모달창
+const inviteIcon = document.querySelector(".gnb-icon.invite")
+const alarmIcon = document.querySelector(".gnb-icon.alarm")
+const inviteModal = document.getElementById("invite-modal")
+const alarmModal = document.getElementById("alarm-modal")
+const modalClose = document.querySelectorAll(".close-modal")
+
+let icons = [inviteIcon, alarmIcon];
+let modals = [inviteModal, alarmModal];
+
+const closeModal = (modal) => {
+    modal.classList.remove("on");
+}
+
+const openModal = (modal) => {
+    modal.classList.toggle("on");
+}
+
+icons.forEach((icon) => {
+    icon.addEventListener("click", (e) => {
+        if (icon.classList.contains("invite")) {
+            openModal(inviteModal);
+            closeModal(alarmModal);
+        }
+        if (icon.classList.contains("alarm")) {
+            openModal(alarmModal);
+            closeModal(inviteModal);
+        }
+    });
+});
+
+modalClose.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        modals.forEach((modal) => {
+            const modalClose = modal.classList.contains("on") ? inviteModal : alarmModal;
+            modalClose.classList.remove("on");
         })
     })
 })
