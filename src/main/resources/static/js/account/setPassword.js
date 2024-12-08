f0ccc683043d7da7403be1a3c120548862c85c5d
김시은 <kimssi9567@gmail.com>
2024-12-09 03:35:23 +0900

    ãdesign: ívariable수ì

--- src/main/resources/static/js/account/setPassword.js
+++ src/main/resources/static/js/account/setPassword.js
@@ -2,8 +2,7 @@ let passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,32}$/;
 const inputPassword = document.getElementById("password")
 const inputPasswordChk = document.getElementById("password-chk")
 const errorWrap = document.querySelector(".message-wrap.error")
-
-console.log("gk")
+const nextBtn = document.querySelector(".primary-btn")
 
 const checkPassword = () => {
     const passwordValue = inputPassword.value
@@ -16,4 +15,13 @@ const checkPassword = () => {
     }
 }
 
+const handleBtnDisabled = (e) => {
+    if (inputPassword.value) {
+        nextBtn.disabled = false;
+    } else {
+        nextBtn.disabled = true;
+    }
+}
+
 inputPasswordChk.addEventListener("input", checkPassword);
+inputPassword.addEventListener("input", handleBtnDisabled);
