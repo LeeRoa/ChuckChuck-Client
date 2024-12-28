2cae7710cddcc7741d05698c68ae81f0f5bf7cf7
hslee <hslee@kcert.co.kr>
2024-12-28 16:59:22 +0900

    접근 권한 변경

--- src/main/java/com/client/chuck/config/SpringSecurityConfig.java
+++ src/main/java/com/client/chuck/config/SpringSecurityConfig.java
@@ -29,7 +29,7 @@ public class SpringSecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/**").hasRole("USER")
+                .requestMatchers("/").hasRole("USER")
                 .anyRequest().permitAll()
                 )
                 // login setting
