9d0bb96bb3afdb27516acfe95187bd92b36ba01b
9567828 <kimssi9567@gmail.com>
2025-01-08 17:57:49 +0900

    관리자페이지 작업중

--- src/main/resources/static/js/admin/workTime.js
+++ src/main/resources/static/js/admin/workTime.js
@@ -1,1 +1,13 @@
-console.log("하이")
+$(function() {
+    $(".select-wrap").click(() => {
+        $('.sel-options').toggleClass("active");
+    });
+
+    $('.sel-options p').each((index, item) => {
+        $(item).click(() => {
+            const selectTeam = $('.select-one')
+            selectTeam.text($(item).text());
+            selectTeam.css('color', 'black')
+        })
+    })
+})
